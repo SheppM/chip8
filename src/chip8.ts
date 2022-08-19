a9abac04255e955e23b23b3f98cca3f62a58c902
SheppM <matthewsheppard256@protonmail.com>
2022-08-19 03:19:47 -0400

    Another 10 operations for chip8

--- src/chip8.ts
+++ src/chip8.ts
@@ -78,5 +78,88 @@ export class Chip8 {
     this.registers[x] = this.registers[y];
   }
 
+  OR_VX_VY(x: number, y: number) {
+    /* Set Vx = Vx OR Vy. 
+       Performs a bitwise OR on the values of Vx and Vy,
+       then stores the result in Vx. A bitwise OR compares the corresponding 
+       bits from two values, and if either bit is 1, then the same bit in the
+       result is also 1. Otherwise, it is 0. */
+    this.registers[x] |= this.registers[y];
+  }
+
+  AND_VX_VY(x: number, y: number) {
+    /* Set Vx = Vx AND Vy.
+       Performs a bitwise AND on the values of Vx and Vy,
+       then stores the result in Vx.
+       A bitwise AND compares the corresponding bits from two values,
+       and if both bits are 1, then the same bit
+       in the result is also 1. Otherwise, it is 0. */
+    this.registers[x] &= this.registers[y];
+  }
+
+  XOR_VX_VY(x: number, y: number) {
+    /* Set Vx = Vx XOR Vy. 
+       Performs a bitwise exclusive OR on the values of Vx and Vy,
+       then stores the resultin Vx. 
+       An exclusive OR compares the corresponding bits from two values,
+       and if the bits are not both the same,
+       then the corresponding bit in the result is set to 1.
+       Otherwise, it is 0. */
+    this.registers[x] ^= this.registers[y];
+  }
+
+  ADD_VX_VY(x: number, y: number) {
+    /* Set Vx = Vx + Vy, set VF = carry.
+       The values of Vx and Vy are added together. If the result is greater
+       than 8 bits (i.e. > 255,) VF is set to 1, otherwise 0.
+       Only the lowest 8 bits of the result are kept, and stored in Vx. */
+    const result = this.registers[x] + this.registers[y];
+    this.registers[0xf] = result > 0xff ? 1 : 0;
+    this.registers[x] = result & 0xff;
+  }
+
+  SUB_VX_VY(x: number, y: number) {
+    /* Set Vx = Vx - Vy, set VF = NOT borrow.
+       If Vx > Vy, then VF is set to 1, otherwise 0.
+       Then Vy is subtracted from Vx, and the results stored in Vx. */
+    this.registers[0xf] = this.registers[x] > this.registers[y] ? 1 : 0;
+    this.registers[x] -= this.registers[y];
+  }
+
+  SHR_VX_VY(x: number, y: number) {
+    /* Set Vx = Vx SHR 1.
+       If the least-significant bit of Vx is 1,
+       then VF is set to 1, otherwise 0. Then Vx is divided by 2. */
+    this.registers[0xf] = this.registers[x] & 1;
+    this.registers[x] >>= 1;
+  }
+
+  SUBN_VX_VY(x: number, y: number) {
+    /* Set Vx = Vy - Vx, set VF = NOT borrow.
+      If Vy > Vx, then VF is set to 1, otherwise 0.
+      Then Vx is subtracted from Vy, and the results stored in Vx. */
+    this.registers[0xf] = this.registers[y] > this.registers[x] ? 1 : 0;
+    this.registers[x] = this.registers[y] - this.registers[x];
+  }
+
+  SHL_VX_VY(x: number, y: number) {
+    /* Set Vx = Vx SHL 1. 
+       If the most-significant bit of Vx is 1, 
+       then VF is set to 1, otherwise to 0. Then Vx is multiplied by 2. */
+    this.registers[0xf] = this.registers[x] & 0x8;
+    this.registers[x] <<= 1;
+  }
+
+  SNE_VX_VY(x: number, y: number) {
+    /* Skip next instruction if Vx != Vy. 
+       The values of Vx and Vy are compared, and if they are not equal, the
+       program counter is increased by 2. */
+    if (this.registers[x] != this.registers[y]) this.pc += 2;
+  }
+  LD_I_ADDR(nnn: number) {
+    /* Set I = nnn. The value of register I is set to nnn. */
+    this.I = nnn;
+  }
+
   executeInstruction(opcode: number) {}
 }
